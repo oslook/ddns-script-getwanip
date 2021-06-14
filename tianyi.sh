80b710e26999eeef3b1871ef5a1bc6d897308cae
Vick SUN <oslook@users.noreply.github.com>
2021-06-14 16:10:28 +0800

    Update tianyi.sh

--- tianyi.sh
+++ tianyi.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-curl 'http://192.168.1.1/cgi-bin/luci' \
+ADDR=http://192.168.1.1
+USERNAME="useradmin"
+PASSWORD="" # add
+COOKIE=/tmp/cookie.txt
+
+curl '${ADDR}/cgi-bin/luci' \
  -H 'Upgrade-Insecure-Requests: 1' \
  -H 'Origin: http://192.168.1.1' \
  -H 'Content-Type: application/x-www-form-urlencoded' \
  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36' \
- -H 'Referer: http://192.168.1.1/cgi-bin/luci' \
- --data-raw 'username=useradmin&psd=zmn4e' \
- --cookie-jar cookie.txt > /dev/null
-#curl -i 'http://192.168.1.1/cgi-bin/luci' --cookie cookie.txt
+ -H 'Referer: ${ADDR}/cgi-bin/luci' \
+ --data-raw 'username=${username}&psd=$pasword{}' \
+ --cookie-jar ${COOKIE} > /dev/null
 
-curl 'http://192.168.1.1/cgi-bin/luci/admin/settings/gwinfo?get=part' --cookie cookie.txt | awk -F "\"" '{print $16}' 
+curl '${ADDR}/cgi-bin/luci/admin/settings/gwinfo?get=part' --cookie ${COOKIE} | awk -F "\"" '{print $16}' 
